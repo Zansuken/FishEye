88e4e1922aed17baa2fa8c91ee0e74a15b4e7e90
Zansuken <doom.sebastien.pro@gmail.com>
2021-12-07 18:26:45 +0100

    feat(photographers): linked datas to home page view

--- Front-End-Fisheye/scripts/pages/index.js
+++ Front-End-Fisheye/scripts/pages/index.js
@@ -1,48 +1,37 @@
+const datas = fetch("./data/photographers.json")
+.then(res => res.json())
+.then( function(data) {
+       
    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        class Photographer {
-            constructor(name, id, city, country, tagline, price, portrait) {
-                this.name = name;
-                this.id = id;
-                this.city = city;
-                this.country = country;
-                this.tagline = tagline;
-                this.price = price;
-                this.portrait = portrait;
-            }
-        }
-        const photographers = []
-        const datas = fetch("./data/photographers.json")
-        .then(res => res.json())
-        .then( function(data) {
-            for (const photograph of data.photographers) {
-                photographers.push(photograph)
-            }
-            return photographers
-        })
-        .catch(err => console.log(err))
-        console.log(photographers);
-
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    // Penser à remplacer par les données récupérées dans le json
+    const photographers = []
+    for (const photographer of data.photographers) {
+        photographers.push(photographer)
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    console.log(photographers);
+    // et bien retourner le tableau photographers seulement une fois
+    return ({
+        photographers: [...photographers, ...photographers, ...photographers]})
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
+
+})
+.catch(err => console.log(err))
